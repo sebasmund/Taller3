d17b5e280b4272d1072d5e7bc3e7ae42190798ea
sebasmund <sebasmundaray@gmail.com>
2023-03-15 21:20:35 -0500

    Segunda tabla farmaceutas
    Se añade la segunda tabla totalmente funcional.

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,21 @@
 $('#NuevoMed').click(
-    function(){
-        var nombre=$('#Nombre').val();
-        var precio=$('#Precio').val();
-        var marca=$('#Marca').val();
-        var presentacion=$('#Presentacion').val();
-        var cantidad=$('#Cantidad').val();
-        $('#Medicamentos tr:last').after('<tr><td>'+nombre+'</td><td>'+precio+'</td><td>'+marca+'</td><td>'+presentacion+'</td><td>'+cantidad+'</td></tr>');
+    function () {
+        var nombre = $('#Nombre').val();
+        var precio = $('#Precio').val();
+        var marca = $('#Marca').val();
+        var presentacion = $('#Presentacion').val();
+        var cantidad = $('#Cantidad').val();
+        $('#Medicamentos tr:last').after('<tr><td>' + nombre + '</td><td>' + precio + '</td><td>' + marca + '</td><td>' + presentacion + '</td><td>' + cantidad + '</td></tr>');
         $('#exampleModal').modal('toggle');
     }
-    );
+);
+$('#NuevoFarm').click(
+    function () {
+        var nombre1 = $('#Nombre1').val();
+        var apellido = $('#Apellido').val();
+        var id = $('#ID').val();
+        var turno = $('#Turno').val();
+        $('#Farmaceutas tr:last').after('<tr><td>' + nombre1 + '</td><td>' + apellido + '</td><td>' + id + '</td><td>' + turno + '</td></tr>');
+        $('#exampleModal').modal('toggle');
+    }
+);
